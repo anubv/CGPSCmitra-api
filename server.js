4ff3d03cb60d23025cfadb71a0296b5b1ca28de8
anubhav <anubvmonty@gmail.com>
2020-05-07 00:05:25 +0530

    used PORT of env variables

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
-const PORT = 3000
+const PORT = process.env.PORT 
 
 app.use(express.json())
 app.use('/', router)
@@ -13,5 +13,5 @@ app.listen(PORT, ()=>{
 router.get('/', (req, res)=>{
 
     res.json({message : 'you are here'})
-    
+
 })
