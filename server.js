345236f0f2ecf1588857f7fa6627c037b58ae76c
anubhav <anubvmonty@gmail.com>
2020-05-06 23:51:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+const PORT = 3000
+
+app.use(express.json())
+app.use('/', router)
+
+app.listen(PORT, ()=>{
+    console.log('listening on ' + PORT)
+})
+
+router.get('/', (req, res)=>{
+
+    res.json({message : 'you are here'})
+    
+})
