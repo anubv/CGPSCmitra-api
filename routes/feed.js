fbcd60025da393d719e9ed28dc7612f4880a4ba1
anubv <anubvmonty@gmail.com>
2020-08-13 11:28:10 +0530

    emp api update

--- routes/feed.js
+++ routes/feed.js
@@ -4,12 +4,28 @@ const feedRouter = express.Router();
 
 
 
-
-
-
-feedRouter.route('/')
-.get((req,res)=>{
-    res.send('this is router page')
+feedRouter.get('/', (req,res)=>{
+    res.json(
+        [{
+           "employee_name" : "mahesh",
+           "employee_age" : "56",
+           "employee_salary" : "5000",
+           "id": 1
+        },
+         {
+            "employee_name" : "ganesh",
+            "employee_age" : "78",
+            "employee_salary" : "3000",
+            "id": 2
+        },
+        {
+            "employee_name" : "suresh",
+            "employee_age" : "45",
+            "employee_salary" : "6500",
+            "id": 3
+        }]
+    
+     )
 })
 
 
